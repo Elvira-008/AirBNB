f0f33e0d82ec7fdb802dae1d0126d99b41c5377a
elviraaa112 <elvira07@icloud.com>
2025-11-26 11:40:24 +0600

    DONE

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('properties/', PropertyListApiview.as_view(), name='property-list'),
     path('properties/<int:pk>/', PropertyDetailAPView.as_view(), name='property-detail'),
-    # path('register/', RegisterView.as_view(), name='register'),
+    #path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('bookings/', BookingListAPView.as_view(), name='booking-create'),
